4160a1bbeb25b96f6161a262fc61100f14acdb7b
Alexis Luengas <alexis.luengas@gmail.com>
2019-05-24 20:29:53 +0200

    Fix script; Fix asciidoc

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,86 +1,90 @@
-
-$('ul.keyboard li a').on('click', function() {
-  return false;
-});
-
-// Keyboard 1
-$(document).on({
-  mouseenter: function () {
-    $("#koverlay1").css('opacity', 0.2);
-  },
-  mouseleave: function () {
-    $("#koverlay1").css('opacity', 0.9);
-  }
-}, "#k1");
-
-// Keyboard 2
-// colors
 var k2colors = {
-  C: "#ff5c00",
-  D: "#fc9200",
-  E: "#ffc600",
-  F: "#b8d30b",
-  G: "#2daebf",
-  A: "#067ccf",
-  B: "#b445d9",
+  C: '#ff5c00',
+  D: '#fc9200',
+  E: '#ffc600',
+  F: '#b8d30b',
+  G: '#2daebf',
+  A: '#067ccf',
+  B: '#b445d9'
 };
 
-// hover keyboard
-$(document).on({
-  mouseenter: function(e) {
-    var pitchClass = $(this).attr('class').split(" ").pop();
-    $("#k2 ul.keyboard.pop li a." + pitchClass).addClass('triggerPulse');
-    $(this).css({
-      'color': k2colors[$(this).html()],
-      'background': "#3d4a53",
-      'text-shadow': '1px 1px 4px black'
-    });
-  },
-  mouseleave: function(e) {
-    var pitchClass = $(this).attr('class').split(" ").pop();
-    $("#k2 ul.keyboard.pop li a." + pitchClass).removeClass('triggerPulse');
-    $(this).css({
-      'color': 'white',
-      'background': "#1b2a34",
-      'text-shadow': '0 -1px #090909'
-    });
-  }
-}, "#koverlay2 li a.box.on");
+$.domReady(() => {
+  $('body').addClass('is-loaded');
 
-// hover note row
-$(document).on({
-  mouseenter: function(e) {
+  $('ul.keyboard li a').on('click', function() {
+    return false;
+  });
 
-    var octnr = $(this).closest('ul').attr('id').slice(-1),
-      sstr,
-      pitchClass = $(this).attr('class').split(" ").pop(),
-      $basenote = $("#koverlay2 li ." + pitchClass);
-    if ($.inArray($(this).html(), ["8", "9", "0"]) > -1) {
-      sstr = $("<sub>" + 7 + "</sub>")
-    } else {
-      sstr = $("<sub>" + octnr + "</sub>")
-    }
-    pitchName = $basenote.html()
-    $basenote.append(sstr);
-    $basenote.css({
-      'color': k2colors[pitchName],
-      'background': "#3d4a53",
-      'text-shadow': '1px 1px 4px black'
+  $(document)
+    .on('mouseenter', '#k1', function() {
+      $('#koverlay1').css('opacity', 0.2);
+    })
+    .on('mouseleave', '#k1', function() {
+      $('#koverlay1').css('opacity', 0.9);
+    })
+    .on('mouseenter', '#koverlay2 li a.box.on', function(e) {
+      var keyEl = $(e.target);
+      var pitchClass = keyEl
+        .attr('class')
+        .split(' ')
+        .pop();
+      $('#k2 ul.keyboard.pop li a.' + pitchClass).addClass('triggerPulse');
+      keyEl.css({
+        color: k2colors[keyEl.textContent],
+        background: '#3d4a53',
+        'text-shadow': '1px 1px 4px black'
+      });
+    })
+    .on('mouseleave', '#koverlay2 li a.box.on', function(e) {
+      var keyEl = $(e.target);
+      var pitchClass = keyEl
+        .attr('class')
+        .split(' ')
+        .pop();
+      $('#k2 ul.keyboard.pop li a.' + pitchClass).removeClass('triggerPulse');
+      keyEl.css({
+        color: 'white',
+        background: '#1b2a34',
+        'text-shadow': '0 -1px #090909'
+      });
+    })
+    .on('mouseenter', '#k2 ul.keyboard.pop li a.box.on', function(e) {
+      var keyEl = $(e.target);
+      var octnr = keyEl
+          .closest('ul')
+          .attr('id')
+          .slice(-1),
+        sstr,
+        pitchClass = keyEl
+          .attr('class')
+          .split(' ')
+          .pop(),
+        $basenote = $('#koverlay2 li .' + pitchClass);
+      if ($.inArray(keyEl.textContent, ['8', '9', '0'])) {
+        sstr = $('<sub>' + 7 + '</sub>');
+      } else {
+        sstr = $('<sub>' + octnr + '</sub>');
+      }
+      pitchName = $basenote.textContent;
+      $basenote.append(sstr);
+      $basenote.css({
+        color: k2colors[pitchName],
+        background: '#3d4a53',
+        'text-shadow': '1px 1px 4px black'
+      });
+    })
+    .on('mouseleave', '#k2 ul.keyboard.pop li a.box.on', function(e) {
+      var keyEl = $(e.target);
+      var pitchClass = keyEl
+        .attr('class')
+        .split(' ')
+        .pop();
+      $basenote = $('#koverlay2 li .' + pitchClass);
+      $('#koverlay2 li .' + pitchClass + ' sub').remove();
+      $basenote.css({
+        color: 'white',
+        background: '#1b2a34',
+        'text-shadow': '0 -1px #090909'
+      });
     });
-    // text-shadow: 0 -1px $keybg;
-  },
-  mouseleave: function(e) {
-    var pitchClass = $(this).attr('class').split(" ").pop();
-    $basenote = $("#koverlay2 li ." + pitchClass);
-    $("#koverlay2 li ." + pitchClass + " sub").remove();
-    $basenote.css({
-      'color': 'white',
-      'background': "#1b2a34",
-      'text-shadow': '0 -1px #090909'
-    });
-  }
-}, "#k2 ul.keyboard.pop li a.box.on");
-
-$.domReady(function() {
-  $("body").addClass("is-loaded");
 });
