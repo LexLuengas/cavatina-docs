b54604bd80bc841052e4d1b1c676f5e9ecdbf4c9
Alexis Luengas <alexis.luengas@gmail.com>
2019-05-30 19:27:28 +0200

    Update file watcher

--- pipeline/watch.sh
+++ pipeline/watch.sh
@@ -1,2 +1,2 @@
 echo 'Compiling asciidoc on every change'
-asciidoctor docs.adoc && fswatch --verbose -0 -d . -e ".*" -i "\\.adoc$" | xargs -0 -n1 -I{} asciidoctor {}
+asciidoctor docs.adoc && fswatch --verbose -0 -d . -e ".*" -i "[^E]\\.adoc$" | xargs -0 -n1 -I{} asciidoctor {}
